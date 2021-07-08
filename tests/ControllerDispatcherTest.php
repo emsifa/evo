336968d648f24ca076eff4182b6e73f91b832409
emsifa <emsifa@users.noreply.github.com>
2021-07-08 12:10:16 +0000

    Fix styling

--- tests/ControllerDispatcherTest.php
+++ tests/ControllerDispatcherTest.php
@@ -3,17 +3,11 @@
 namespace Emsifa\Evo\Tests;
 
 use Emsifa\Evo\ControllerDispatcher;
-use Emsifa\Evo\Evo;
 use Emsifa\Evo\Tests\Samples\Controllers\SampleController;
 use Emsifa\Evo\Tests\Samples\DTO\PostStuffDTO;
-use Exception;
 use Illuminate\Container\Container;
-use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Route;
-use Illuminate\Routing\Router;
 use Illuminate\Validation\ValidationException;
-use ReflectionMethod;
 
 class ControllerDispatcherTest extends TestCase
 {
@@ -75,6 +69,7 @@ class ControllerDispatcherTest extends TestCase
             $errors = $e->validator->errors()->toArray();
             $this->assertEquals(1, count($errors));
             $this->assertArrayHasKey('age', $errors);
+
             throw $e;
         }
     }
