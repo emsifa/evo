3dd18524aa4ce13a76a31b4fc1488da6106748c8
Muhammad Syifa <emsifa@gmail.com>
2021-07-20 20:26:13 +0700

    Add cookie attribute

---
+++ tests/Http/CookieTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Emsifa\Evo\Tests\Http;
+
+use Emsifa\Evo\Http\Cookie;
+use Emsifa\Evo\Tests\Samples\Casters\HalfIntCaster;
+use Emsifa\Evo\Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use ReflectionNamedType;
+use ReflectionParameter;
+
+class CookieTest extends TestCase
+{
+    public function testGetValueFromCookie()
+    {
+        /**
+         * @var \ReflectionParameter
+         */
+        $reflection = $this->getMockReflectionParam('token', 'string');
+        $request = $this->makeRequestWithCookies(["token" => "qwerty"]);
+        $param = new Cookie();
+        $result = $param->getRequestValue($request, $reflection);
+
+        $this->assertEquals("qwerty", $result);
+    }
+
+    public function testGetValueFromCookieWithDifferentKeyName()
+    {
+        /**
+         * @var \ReflectionParameter
+         */
+        $reflection = $this->getMockReflectionParam('foo', 'string');
+        $request = $this->makeRequestWithCookies(["token" => "lorem-ipsum"]);
+        $param = new Cookie('token');
+        $result = $param->getRequestValue($request, $reflection);
+
+        $this->assertEquals('lorem-ipsum', $result);
+    }
+
+    public function testGetValueFromCookieWithCaster()
+    {
+        /**
+         * @var \ReflectionParameter
+         */
+        $reflection = $this->getMockReflectionParam('number', 'int');
+        $request = $this->makeRequestWithCookies(["number" => "10"]);
+        $param = new Cookie('number', caster: HalfIntCaster::class);
+        $result = $param->getRequestValue($request, $reflection);
+
+        $this->assertEquals(5, $result);
+        $this->assertEquals('integer', gettype($result));
+    }
+
+    public function testValidationSucceed()
+    {
+        /**
+         * @var \ReflectionParameter
+         */
+        $reflection = $this->getMockReflectionParam('number', 'int');
+        $request = $this->makeRequestWithCookies(["number" => "120"]);
+        $param = new Cookie('number', rules: 'numeric');
+
+        $this->assertNull($param->validateRequest($request, $reflection));
+    }
+
+    public function testValidationError()
+    {
+        /**
+         * @var \ReflectionParameter
+         */
+        $reflection = $this->getMockReflectionParam('number', 'int');
+        $request = $this->makeRequestWithCookies(["number" => "im-not-a-number"]);
+        $param = new Cookie('number', rules: 'numeric');
+
+        $this->expectException(ValidationException::class);
+        $param->validateRequest($request, $reflection);
+    }
+
+    private function makeRequestWithCookies(array $cookies): Request
+    {
+        return new Request(cookies: $cookies);
+    }
+
+    private function getMockReflectionParam($name, string $typeName = '', $allowsNull = false)
+    {
+        if ($typeName) {
+            $type = $this->createStub(ReflectionNamedType::class);
+            $type->method('getName')->willReturn($typeName);
+            $type->method('allowsNull')->willReturn($allowsNull);
+        }
+
+        $reflection = $this->createStub(ReflectionParameter::class);
+        $reflection->method('getName')->willReturn($name);
+        $reflection->method('getType')->willReturn($typeName ? $type : null);
+
+        return $reflection;
+    }
+}
