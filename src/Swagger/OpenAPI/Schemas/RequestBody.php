86ec475122b58a82bbf51b575b39e51f966fec14
Muhammad Syifa <emsifa@gmail.com>
2021-07-26 17:59:57 +0700

    fix: RequestBody schema property

--- src/Swagger/OpenAPI/Schemas/RequestBody.php
+++ src/Swagger/OpenAPI/Schemas/RequestBody.php
@@ -2,29 +2,12 @@
 
 namespace Emsifa\Evo\Swagger\OpenApi\Schemas;
 
-use Illuminate\Contracts\Support\Arrayable;
-
-class RequestBody implements Arrayable
+class RequestBody extends BaseSchema
 {
     public ?string $description = null;
-    public bool $required = true;
-    public string $contentType = "*/*";
-    public string $contentSchema;
-
-    public function toArray()
-    {
-        $schema = [];
-        if ($this->description) {
-            $schema['description'] = $this->description;
-        }
-        $schema['required'] = $this->required;
-        $schema['url'] = $this->url;
-        $schema['content'][$this->contentType] = [
-            'schema' => [
-                '$ref' => $this->contentSchema,
-            ],
-        ];
-
-        return $schema;
-    }
+    public ?bool $required = null;
+    /**
+     * @var MediaType[]
+     */
+    public array $content = [];
 }
