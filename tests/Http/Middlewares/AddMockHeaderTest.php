4beec219b21ab876ff8d129639b80b759cb4335b
Muhammad Syifa <emsifa@gmail.com>
2021-08-23 20:12:35 +0700

    Test AddMockHeader middleware

---
+++ tests/Http/Middlewares/AddMockHeaderTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Emsifa\Evo\Tests\Http\Middlewares;
+
+use Emsifa\Evo\Http\Middlewares\AddMockHeader;
+use Emsifa\Evo\Http\Response\Mock;
+use Emsifa\Evo\Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AddMockHeaderTest extends TestCase
+{
+    public function testItShouldAddEvoMockHeaderToResponse()
+    {
+        $this->app->bind(Mock::class, function() {
+            $mock = new Mock();
+            $mock->setClassName("FooBar");
+            return $mock;
+        });
+
+        $middleware = new AddMockHeader($this->app);
+        /**
+         * @var Response
+         */
+        $response = $middleware->handle(new Request(), function() {
+            return new Response();
+        });
+
+        $mockName = $response->headers->get('Evo-Mock');
+        $this->assertEquals("FooBar", $mockName);
+    }
+}
